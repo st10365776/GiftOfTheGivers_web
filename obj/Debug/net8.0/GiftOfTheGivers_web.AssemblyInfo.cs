111aad95df12aa4e64fe779d18a985cf0a82124c
Ethan Grunewald <egrunewald0710@gmail.com>
2026-08-25 14:14:33 +0200

    Add responsive hero, impact, statistics, mission, and donation sections - Update navigation and footer with organization-focused links and CTAs - Add branded imagery, logo assets, and comprehensive responsive styling - Remove unused Privacy action and default template references

--- obj/Debug/net8.0/GiftOfTheGivers_web.AssemblyInfo.cs
+++ obj/Debug/net8.0/GiftOfTheGivers_web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiftOfTheGivers_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0d3f3e646e21258fc062a255d6756c6980abf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiftOfTheGivers_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiftOfTheGivers_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
